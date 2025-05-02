5107cc26932636afa5443d75143def2be3d6ed7f
Ana Barbiero <anacarolinabarbiero@gmail.com>
2025-05-02 16:55:42 -0400

    Revert "Commit - Estrutura de codigo"
    This reverts commit a0896dfdc034cb1a5b1af8e4a69336546f02d055.

--- FrontEnd/src/main.jsx
+++ FrontEnd/src/main.jsx
@@ -1,19 +1,10 @@
-import React from "react"; 
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './Index.css'
+import LoginForm from './LoginForm.jsx'
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./assets/css/index.css";
-import LoginForm from "./pages/LoginForm.jsx";
-import RegisterForm from "./pages/RegisterForm.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="/registro" element={<RegisterForm />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <LoginForm />
+  </StrictMode>,
+)
